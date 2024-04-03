22145f2bfdb2862c268535874b73b002b88b8667
zareenaahsan <165138314+zareenaahsan@users.noreply.github.com>
2024-04-03 23:58:33 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+import inquirer from "inquirer";
+//Asking Question from users through Inquirer
+let answers = await inquirer.prompt([
+    { message: "Enter First Number", type: "number", name: "firstNumber" },
+    { message: "Enter Second Number", type: "number", name: "secondNumber" },
+    {
+        message: "Select one operator to perform Operations",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+    },
+]);
+//console.log(answers);
+// conditional statements If-Else
+if (answers.operator === "Addition") {
+    console.log(answers.firstNumber + answers.secondNumber);
+}
+else if (answers.operator === "Subtraction") {
+    console.log(answers.firstNumber - answers.secondNumber);
+}
+else if (answers.operator === "Multiplication") {
+    console.log(answers.firstNumber * answers.secondNumber);
+}
+else if (answers.operator === "Division") {
+    console.log(answers.firstNumber / answers.secondNumber);
+}
+else {
+    console.log("Invalid Input");
+}
